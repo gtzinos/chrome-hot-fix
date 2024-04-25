bfa2c9f6f3f3ca87ee3b7c2c910fc450a857b44d
George Tzinos <geotzinos@gmail.com>
2024-04-25 09:35:44 +0400

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,6 +1,36 @@
-document.addEventListener('DOMNodeInserted', function () {
-    const elements = document.querySelectorAll('[aria-owns]');
-    elements.forEach(element => {
-        element.removeAttribute('aria-owns');
-    });
-});
+
+setTimeout(() => {
+    // document.addEventListener('DOMNodeInserted', function () {
+    //     try {
+    //         const elements = document.querySelectorAll('[aria-owns]');
+    //         elements.forEach(element => {
+    //             element.removeAttribute('aria-owns');
+    //         });
+    //     } catch (e) {
+    //         console.error(e);
+    //     }
+    // });
+
+    function callback() {
+        try {
+            const elements = document.querySelectorAll('[aria-owns]');
+            elements.forEach(element => {
+                element.removeAttribute('aria-owns');
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    function retry () {
+        // Create an observer instance linked to the callback function
+        const observer = new MutationObserver(callback);
+    
+        const targetNode = document.getElementsByTagName("html")?.[0];
+        const config = { attributes: true, childList: true, subtree: true };
+    
+        // Start observing the target node for configured mutations
+        observer.observe(targetNode, config);
+    }
+    retry();
+}, 100);
